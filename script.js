1260ce87dc5023bf65e9d9f7f65c454ddba2b4e3
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2023-08-22 15:27:48 +0530

    script.js

--- script.js
+++ script.js
@@ -1,69 +1,106 @@
-// arrow - id name of link tag of style sheet
-// modeIcon - id name used for the font-awesome icon(default icon used fa-sun)
-const themeChanger = document.querySelector('#arrow');
-const modeIcon = document.querySelector('#modeIcon');
+const themechanger = document.querySelector('#arrow');
+const themeChangeIcon = document.querySelector('#modeIcon');
+const resume = document.querySelector('.downloadCV');
+// Function for changing mode using icons
+if (themeChangeIcon) {
+    themeChangeIcon.addEventListener('click', changeTheme);
+}
 
-// Functions for toggle mode and icon
-function toggleMode() {
-    if (themeChanger.href.endsWith('darkstyle.css')) {
-        themeChanger.href = "lightstyle.css";
+function changeTheme() {
+    if (themechanger.href.endsWith('darkstyle.css')) {
+        themechanger.href = 'lightstyle.css';
         localStorage.setItem('mode', 'light');
-        if (modeIcon) {
-            modeIcon.classList.replace('fa-moon', 'fa-sun');
-        }
+        themeChangeIcon.classList.replace('fa-moon', 'fa-sun');
     } else {
-        themeChanger.href = "darkstyle.css";
+        themechanger.href = 'darkstyle.css';
         localStorage.setItem('mode', 'dark');
-        if (modeIcon) {
-            modeIcon.classList.replace('fa-sun', 'fa-moon');
-        }
+        themeChangeIcon.classList.replace('fa-sun', 'fa-moon');
     }
 }
 
-// Event listener for clicking the mode icon
-if (modeIcon) {
-    modeIcon.addEventListener('click', toggleMode);
-}
-
-
-// Apply user's choice mode for the rest of the pages
 document.addEventListener('DOMContentLoaded', () => {
     const savedMode = localStorage.getItem('mode');
     if (savedMode === 'dark') {
-        themeChanger.href = "darkstyle.css";
-        modeIcon.classList.replace('fa-sun', 'fa-moon');
+        themechanger.href = 'darkstyle.css';
+        themeChangeIcon.classList.replace('fa-sun', 'fa-moon');
     } else {
-        themeChanger.href = "lightstyle.css";
-        modeIcon.classList.replace('fa-moon', 'fa-sun');
+        themechanger.href = 'lightstyle.css';
+        themeChangeIcon.classList.replace('fa-moon', 'fa-sun');
     }
 });
+const hiremeButton = document.querySelector('.hireme');
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOM content loaded.");
+hiremeButton.addEventListener('click', function(){
+    alert("Whoa, slow down there, eager beaver! While I'm flattered you're ready to hire me, let's take a moment to make sure you're not missing out on the adventure of exploring first. It's like eating dessert before the main course – tempting, but let's savor the journey!");
+
+    // Remove the hireme button
+    hiremeButton.style.display = 'none';
+});
+
+resume.addEventListener('click', function(){
+    window.open("https://drive.google.com/file/d/1UqssJbd9H9-9BxiJrmbnnGTK9zB6edgE/view?usp=sharing", "_blank");
+});
+
+function sendWhatsAppMessage() {
     const whatsapp = document.getElementById('whatsapplink');
     const phoneNumber = "+918979991191";
     const defaultMessage = "Hello!%20I%20came%20across%20your%20portfolio%20website%20and%20I'm%20interested%20in%20learning%20more%20about%20your%20work.%20Could%20you%20please%20provide%20me%20with%20more%20information?%20Thank%20you!";
     const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${defaultMessage}`;
     whatsapp.href = whatsappUrl;
-  });
+}
 
-document.addEventListener('DOMContentLoaded', function(){
+function sendEmail(){
     const emailMsg = document.getElementById('email');
     const emailId = 'sudhamshasagar@gmail.com';
     const subject = 'Inquiry from Portfolio Website';
     const message = "Hello!%20I%20came%20across%20your%20portfolio%20website%20and%20I'm%20interested%20in%20learning%20more%20about%20your%20work.%20Could%20you%20please%20provide%20me%20with%20more%20information?%20Thank%20you!";
     emailMsg.href = `mailto:garg.astha1022@gmail.com?subject=${subject}&body=${message}`;
-});
+}
+
+document.addEventListener('DOMContentLoaded', sendWhatsAppMessage);
 
 
-// Functionality for Download CV Button
-const downLoad = document.querySelector('.downloadCV');
+function openPage(page) {
+    window.location.href = page + '.html';
+}
 
-downLoad.addEventListener('click', function(){
-  window.open("https://drive.google.com/file/d/1UqssJbd9H9-9BxiJrmbnnGTK9zB6edgE/view?usp=sharing", '_blank');
-});
-const hireBtn = document.querySelector('.hireme');
-hireBtn.addEventListener('click', function(){
-  alert(`Whoa, slow down there, eager beaver! While I'm flattered you're ready to hire me, let's take a moment to make sure you're not missing out on the adventure of exploring first.It's like eating dessert before the main course  tempting, but let's savor the journey!`);
-  hireBtn.style.display = 'none';
-});
+function startRecognition() {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript.toLowerCase();
+        if (transcript.includes('dark mode')) {
+            changeTheme(); // Call the changeTheme() function to switch to dark mode
+        }else if (transcript.includes('home')) {
+            openPage('index'); // Call the function to open the About page
+        } else if (transcript.includes('message')) {
+            sendWhatsAppMessage() // Call the function to open the About page
+        }else if (transcript.includes('email')) {
+            sendEmail() // Call the function to open the About page
+        } else if (transcript.includes('education') || transcript.includes('explore')) {
+            openPage('About'); // Call the function to open the About page
+        } else if (transcript.includes('experience') || transcript.includes('awards')) {
+            openPage('experience');
+        } else if (transcript.includes('certificate')) {
+            openPage('certifications');
+        } else if (transcript.includes('contact')) {
+            openPage('contact');
+        } else if (transcript.includes('light mode')) {
+            changeTheme(); // Call the changeTheme() function to switch to light mode
+        }
+    };
+
+    recognition.onend = () => {
+        startRecognition(); // Restart the recognition loop after it ends
+    };
+
+    recognition.start();
+}
+
+// Function for changing mode using voice command
+if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+    startRecognition();
+} else {
+    console.log('Speech recognition is not supported in this browser.');
+}
